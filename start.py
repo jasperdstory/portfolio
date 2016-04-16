1d9b1be4a3813e7263125b5e5770d263bad57875
Jasper Diego Story <jasperdstory@gmail.com>
2016-04-15 21:12:25 -0500

    Pre-Deployment Fixes

--- start.py
+++ start.py
@@ -1,31 +1,39 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
-from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 manager = Manager(app)
-bootstrap = Bootstrap(app)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/B')
+def B():
+    return render_template('B.html')
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/C')
+def C():
+    return render_template('C.html')
+
+@app.route('/D')
+def D():
+    return render_template('D.html')
+
+@app.route('/E')
+def E():
+    return render_template('E.html')
+
 
 
 if __name__ == '__main__':
