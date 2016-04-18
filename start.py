b1748e6a0584d2286548752a2644a88c3addf49e
Jasper Diego Story <jasperdstory@gmail.com>
2016-04-18 13:16:40 -0500

    horizontal navigation

--- start.py
+++ start.py
@@ -16,23 +16,13 @@ def internal_server_error(e):
 def index():
     return render_template('index.html')
 
-@app.route('/B')
-def B():
-    return render_template('B.html')
+@app.route('/portfolio')
+def port():
+    return render_template('portfolio.html')
 
 @app.route('/C')
 def C():
     return render_template('C.html')
-
-@app.route('/D')
-def D():
-    return render_template('D.html')
-
-@app.route('/E')
-def E():
-    return render_template('E.html')
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
