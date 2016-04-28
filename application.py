97d5cc1c4c895afc3002f00a350bf6caff4e4613
Jasper Diego Story <jasperdstory@gmail.com>
2016-04-28 11:23:22 -0500

    particle background

--- application.py
+++ application.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template
 from flask.ext.script import Manager
 from flask.ext.assets import Environment, Bundle
 from flask.ext.compress import Compress
-from flask.ext.cdn import CDN
 
-application = Flask(__name__)
+# from flask.ext.cdn import CDN
 
-assets = Environment(application)
-assets.url = application.static_url_path
+app = Flask(__name__)
+
+assets = Environment(app)
+assets.url = app.static_url_path
 # assets.append_path('assets')
 
 scss = Bundle('mobile-nav.scss', 'desktop-nav.scss', 
@@ -16,50 +17,51 @@ scss = Bundle('mobile-nav.scss', 'desktop-nav.scss',
 			  filters='pyscss, cssmin', output='all.css')
 assets.register('scss_all', scss)
 
-js = Bundle('nav.js', 'case-nav.js', 
+js = Bundle('nav.js', 'case-nav.js', 'journey.js',
             filters='jsmin', output='all.js')
 assets.register('js_all', js)
 
 COMPRESS_MIMETYPES = ['text/html', 'text/css', 
                       'application/javascript', 'application/x-font-opentype']
-Compress(application)
-
-application.config['CDN_DOMAIN'] = 'dn27tkn48vqxi.cloudfront.net'
-CDN(application)
+Compress(app)
 
 
-@application.errorhandler(404)
+# application.config['CDN_DOMAIN'] = 'dn27tkn48vqxi.cloudfront.net'
+# CDN(application)
+
+
+@app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-@application.errorhandler(500)
+@app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@application.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@application.route('/sixthings')
+@app.route('/sixthings')
 def sixthings():
     return render_template('sixthings.html')
 
-@application.route('/news')
+@app.route('/news')
 def news():
     return render_template('news.html')
 
-@application.route('/dashboards')
+@app.route('/dashboards')
 def dashboards():
     return render_template('dashboards.html')
 
-@application.route('/journeys')
+@app.route('/journeys')
 def journeys():
     return render_template('journeys.html')
 
-@application.route('/googleapps')
+@app.route('/googleapps')
 def googleapps():
     return render_template('googleapps.html')
 
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
